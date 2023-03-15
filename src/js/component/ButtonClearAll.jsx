b8ec478bdd788c87bfb336d2cfee2882c7bfb90e
retori8 <reciclee@gmail.com>
2023-03-15 01:52:33 +0000

    async ok

--- src/js/component/ButtonClearAll.jsx
+++ src/js/component/ButtonClearAll.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function ButtonClearAll() {
+export default function ButtonClearAll(props) {
   return (
     <div id="clear">
-    <button type="button" className="btn btn-outline-light btn-lg"  onClick={() => {props.deleteAll(list)}}>
+    <button type="button" className="btn btn-outline-light btn-lg"  onClick={() => {props.deleteAll()}}>
       CLEAR ALL
     </button>
     </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,22 +8,23 @@ import Input from "./Imput.jsx";
 import ButtonClearAll from "./ButtonClearAll.jsx";
 
 const Home = () => {
-  const [task, setTask] = useState({ label: text, done: false });
+
   const [text, setText] = useState("");
   const [list, setList] = useState([]);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
-    getList();
+    initList();
   }, []);
 
   function handleSubmit(e) {
     e.preventDefault();
+    let task = { label: "", done: false }
     task.label = text;
     let newlist = [...list, task];
     setList(newlist);
     addTaskList(newlist);
     setText("");
+    console.log(newlist, "este es el newlist")
   }
 
   const addTaskList = async (task) => {
@@ -40,8 +41,6 @@ const Home = () => {
         options
       );
       const data = await response.json();
-      console.log("tarea guardada");
-      console.log(data);
       if (data.id) {
         getList();
       }
@@ -54,7 +53,7 @@ const Home = () => {
     const deleteTask = [...list];
     deleteTask.splice(i, 1);
     setList(deleteTask);
-    updateTaskList(list);
+    updateTaskList(deleteTask);
   }
 
   const updateTaskList = async (task) => {
@@ -71,8 +70,6 @@ const Home = () => {
         options
       );
       const data = await response.json();
-      console.log("update");
-      console.log(data);
       if (data.id) {
         setList((prevState) => prevState.filter((list) => list.id !== id));
         getList();
@@ -82,30 +79,56 @@ const Home = () => {
     }
   };
 
-  const getList = () => {
-    fetch("https://assets.breatheco.de/apis/fake/todos/user/carorb", {
+  const getList = async () => {
+      const options = {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
-    })
-      .then((resp) => {
-        console.log(resp);
-        if (resp.status === 404) throw Error("Not Found");
-        return resp.json();
-      })
-      .then((data) => {
-        console.log(data);
-        if (data.msg) {
-          createList();
-        } else {
-          setList(data);
-        }
-        console.log(data, "ESTA ES LA DATA");
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
+    };
+    try {
+      const response = await fetch(
+        "https://assets.breatheco.de/apis/fake/todos/user/carorb",
+        options
+      );
+
+      if (response.status == 200) {
+        const respuesta = await response.json ();
+        setList(respuesta)
+        return(respuesta)
+      } else {
+        return(false)
+      }
+
+    } catch (error) {
+      console.log(error);
+      return(false)
+    }
+
+
+    // fetch("https://assets.breatheco.de/apis/fake/todos/user/carorb", {
+    //   method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // })
+    //   .then((resp) => {
+    //     console.log(resp);
+    //     if (resp.status === 404) throw Error("Not Found");
+    //     return resp.json();
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //     if (data.msg) {
+    //       createList();
+    //     } else {
+    //       setList(data);
+    //     }
+    //     console.log(data, "ESTA ES LA DATA");
+    //   })
+    //   .catch((error) => {
+    //     console.log(error.message);
+    //   });
   };
 
   const deleteAll = async () => {
@@ -121,43 +144,51 @@ const Home = () => {
         options
       );
       if (response.status == 200) {
-        getList([]);
-        setError(null);
-      } else {
-        setError("Error al eliminar");
+        console.log("k")
+        setList([]);
       }
     } catch (error) {
       console.log(error);
     }
   };
 
-  //   const createList = async (task) => {
-  //     const options = {
-  //         method: 'POST',
-  //         body: JSON.stringify(task),
-  //         headers: {
-  //             'Content-Type': 'application/json'
-  //         }
-  //     }
-  //     try {
-  //         const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/carorb")
-  //         const data = await response.json();
-  //         console.log("Note Saved");
-  //         console.log(data, "esta es la data");
-  //         if(data.id){
-  //           getList()
-  //         }
-  //     } catch (error) {
-  //         console.log(error);
-  //     }
-  // }
+  async function initList (){
+    let get = await getList()
+    if(get == false){
+      await createList()
+      await getList()
+    };
+
+  };
+
+     const createList = async (task) => {
+       const options = {
+          method: 'POST',
+          body: JSON.stringify([]),
+          headers: {
+              'Content-Type': 'application/json'
+          }
+      }
+      try {
+        console.log(options)
+          const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/carorb", options)
+          const data = await response.json();
+          console.log("Note Saved");
+          console.log(data, "esta es la data");
+          if(data.id){
+            // getList()
+          }
+      } catch (error) {
+          console.log(error);
+      }
+  }
 
   console.log(list);
 
   return (
     <Card>
       <Input handleSubmit={handleSubmit} text={text} setText={setText} />
-      <ButtonClearAll deleteAll={deleteAll} list={list}/>
+      <ButtonClearAll deleteAll={deleteAll}/>
       <List>
         {list.length > 0 ? (
           list.map((task, i) => {
@@ -165,7 +196,7 @@ const Home = () => {
               <ElementsList
                 handleDelete={handleDelete}
                 task={task}
-                i={i.task}
+                i={i}
                 key={i}
               />
             );
